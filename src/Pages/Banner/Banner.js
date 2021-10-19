7e9d9b4650f690f4b2cc77d7e94263163d59c599
Tofazzal322 <tofazzal.tk322@gmail.com>
2021-10-19 16:09:53 +0400

    doctors edited

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -6,43 +6,17 @@ import bannerImg3 from "../../Images/Banner/04.jpg"
 
 const Banner = () => {
     return (
-        <Carousel variant="dark" fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={bannerImg1}
-      alt="First slide"
-    />
-    {/* <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={bannerImg2}
-      alt="Second slide"
-    />
-
-    {/* <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-    src={bannerImg3}
-      alt="Third slide"
-    />
-
-    {/* <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-</Carousel>
+      <Carousel variant="dark" fade>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bannerImg1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bannerImg2} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bannerImg3} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
     );
 };
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import useData from '../../hooks/useData';
-import Footer from '../../Shared/Footer/Footer';
-import MenuBar from '../../Shared/MenuBar/MenuBar';
-import Banner from '../Banner/Banner';
-import Login from '../Login/Login';
-import Service from '../Service/Service';
+// import useData from '../../hooks/useData';
+import Banner from "../Banner/Banner";
+import Service from "../Service/Service";
 
 const Home = () => {
-
-    const { data } = useData()
-    // console.log(data.length);
-    return (
-        <div>
-            
-            {/* <MenuBar></MenuBar> */}
-            
-            <Banner />
-            <Service></Service>
-
-            {/* <Login/> */}
-            {/* <Footer></Footer> */}
-        </div>
-    );
+//   const { data } = useData();
+  return (
+    <div>
+      <Banner />
+      <Service></Service>
+    </div>
+  );
 };
 
 export default Home;
